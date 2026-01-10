821a2071657856a0a3c334e3d1252fbe96b4028a
Andenis55 <aldensinnovations@gmail.com>
2026-01-10 04:57:48 -0800

    Add configuration settings for ShopUp

--- js/core/config.js
+++ js/core/config.js
@@ -1,1 +1,22 @@
+// /js/core/config.js
+(function () {
+  "use strict";
 
+  const CONFIG = Object.freeze({
+    storage: {
+      CART_KEY: "cart",
+      AUTH_TOKEN_KEY: "authToken",
+      CURRENT_USER_KEY: "currentUser",
+      ROLE_KEY: "role",
+    },
+    ui: {
+      FALLBACK_IMAGE: "../images/placeholder.png",
+      DASHBOARD_PRODUCTS_LIMIT: 12,
+    },
+    cart: {
+      FLAT_SHIPPING_GHS: 20,
+    },
+  });
+
+  window.ShopUpConfig = CONFIG;
+})();
