1882015357bdc74c9f76b658dab1ed75756d89d0
Andenis55 <aldensinnovations@gmail.com>
2026-02-06 15:41:19 -0800

    Refactor Supabase config by removing extra whitespace
    Removed whitespace between the Supabase URL and ANON KEY.

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -6,7 +6,7 @@
   const SUPABASE_URL = "https://brbewkxpvihnsrbrlpzq.supabase.co";
   const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyYmV3a3hwdmlobnNyYnJscHpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMTI4OTAsImV4cCI6MjA3ODY4ODg5MH0.SfZMbpxsNHTgoXIvn9HZnXSZAQnCSjKNpAnH4vLVVj4";
-  
+   
   const DEBUG = false;
 
   function log(...args) {
